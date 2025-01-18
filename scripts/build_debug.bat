7d6c65cac3397d8d75903126aad4af65f632b022
admin <admin@noreply.com>
2025-01-18 11:32:14 +0800

    fix qt debug proj

--- scripts/build_debug.bat
+++ scripts/build_debug.bat
@@ -2,7 +2,7 @@
 
 set VC_DIR=D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
 set MSDK_DIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A
-set QT_DIR=D:\Qt\Qt5.7.1\5.7\msvc2015
+set QT_DIR=D:\ProgramData\Qt\Qt5.7.1\5.7\msvc2015
 set WIN_KIT=C:\Program Files (x86)\Windows Kits\10
 set INCLUDE=%VC_DIR%\include;%MSDK_DIR%\Include;%WIN_KIT%\Include\10.0.10150.0\ucrt
 set LIB=%MSDK_DIR%\Lib;%VC_DIR%\lib;%WIN_KIT%\Lib\10.0.10150.0\ucrt\x86
